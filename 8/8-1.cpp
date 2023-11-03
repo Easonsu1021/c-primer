ed9f69cf106c28ed8046fd3074a1c7fb42b562bc
eason <eason891021@gmail.com>
2023-11-03 20:29:24 +0800

    git problem solved

--- 8/8-1.cpp
+++ 8/8-1.cpp
@@ -7,11 +7,8 @@ using namespace std;
 istream& s( istream& in){
     string str;
     while(in >> str){
-<<<<<<< HEAD
         cout << str << endl;    
-=======
         cout << str << endl;
->>>>>>> 9744a739bd194b65dd47663a684105c62dce2092
         cout << in.eof() << endl;
         cout << in.fail() << endl;
         cout << in.bad() << endl;
