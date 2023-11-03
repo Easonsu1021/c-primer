8bba7c22e01f378b0c49278962089c5cb016aef3
eason <eason891021@gmail.com>
2023-11-03 19:45:04 +0800

    8-1 finish

---
+++ 8/8-1.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+using namespace std;
+
+
+
+istream& s( istream& in){
+    string str;
+    while(in >> str){
+        cout << str << endl;
+        cout << in.eof() << endl;
+        cout << in.fail() << endl;
+        cout << in.bad() << endl;
+        cout << in.good() << endl;
+        cout << in.rdstate() << endl;
+    }
+    in.clear(); // 重設資料流
+    return in;
+}
+
+
+int main(){    
+  
+s(cin);
+   
+    
+    
+}
